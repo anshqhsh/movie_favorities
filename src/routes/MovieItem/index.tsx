401a2b496b945b87fc8d3b27d327708a8c843bae
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-05-14 00:17:10 +0900

    error handling

--- src/routes/MovieItem/index.tsx
+++ src/routes/MovieItem/index.tsx
@@ -6,15 +6,17 @@ import styles from './movieItem.module.scss'
 
 const MovieItem = (item: IMovieItem) => {
   const [onFavorite, setOnFavorite] = useState(false)
+  const { Title, Year } = item
+  console.log('', item)
 
-  console.log(item)
-
+  // 이부분을 localstorage에서 관리해야할 듯
   const OnFavoriteHandler = () => {
     setOnFavorite((_onFavorite) => !_onFavorite)
   }
   return (
-    <div className={styles.movieContainer}>
-      <h1>정보</h1>
+    <div className={styles.movieItem}>
+      {Title}
+      {Year}
     </div>
   )
 }
--- src/routes/MovieSearch/index.tsx
+++ src/routes/MovieSearch/index.tsx
@@ -7,30 +7,44 @@ import styles from './movieSearch.module.scss'
 
 const MovieSearch = () => {
   const [search, setSearch] = useState('') // input에서 값을 받아 검색
-  const [movieData, setMovieData] = useState<IMovieItem[]>()
+  const [movieData, setMovieData] = useState<IMovieItem[]>([])
+  const [error, setError] = useState('')
+  console.log('', movieData)
 
   const handleAddClick = () => {} // *** search 기능 구현
 
   useEffect(() => {
     getGripMovieApi({
-      s: 'ion', // 검색어
+      s: 'movie', // 검색어
       apikey: '1f3dc839',
       page: 2,
-    }).then((res) => {
-      setMovieData(res.data.Search)
     })
+      .then((res) => {
+        console.log(res)
+        setMovieData(res.data.Search)
+      })
+      .catch(onError)
   }, [])
+
+  // error check
+  const onError = (err: any) => {
+    setError(err.toString())
+    setTimeout(() => {
+      setError('')
+    }, 3000)
+  }
 
   const formChangeHandler = (e: any) => {
     setSearch(e.currentTarget.value)
   }
-  console.log(movieData)
+
   return (
     <div className={styles.movieContainer}>
       <h1>MovieSearch</h1>
       <form className={styles.searchForm}>
         <input type='text' placeholder='Search Movie' onChange={formChangeHandler} />
       </form>
+      {error && <div>{error}</div>}
       {movieData?.map((item) => (
         <MovieItem key={item.imdbID} {...item} />
       ))}
